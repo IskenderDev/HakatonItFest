be330ced792ed73d815a1389118c26166648bd96
IskenderDev <iskenderkadyrjanov@gmail.com>
2024-11-21 14:33:57 +0600

    added antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Button } from "antd";
+
 export default function App() {
   return (
+    <>
     <h1 className="text-3xl text-pink-500">
       Hello world ! 
     </h1>
+    <Button/></>
   )
 }
