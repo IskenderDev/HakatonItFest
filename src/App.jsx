8972a2407a67ebba60db2c358266d18cff74d067
Turusbekova-Guliza <134058547+Turusbekova-Guliza@users.noreply.github.com>
2024-11-21 14:51:05 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Button } from "antd";
+import Header from './widjets/header/Header'
 
 export default function App() {
   return (
     <>
     <h1 className="text-3xl text-pink-500">
-      Hello world ! 
+      Hello world !
     </h1>
-    <Button/></>
+    <Button/>
+    <Header />
+    </>
   )
 }
